65a75b2b4e7c79c3c942b20ce84171af40595023
pierosavi <piero@pierosavi.it>
2019-08-31 15:03:36 +0200

    Built merge

---
+++ dist/module.js
@@ -0,0 +1,16 @@
+"use strict";
+
+System.register(["./imageit_ctrl"], function (_export, _context) {
+  "use strict";
+
+  var ImageItCtrl;
+  return {
+    setters: [function (_imageit_ctrl) {
+      ImageItCtrl = _imageit_ctrl.ImageItCtrl;
+    }],
+    execute: function () {
+      _export("PanelCtrl", ImageItCtrl);
+    }
+  };
+});
+//# sourceMappingURL=module.js.map
