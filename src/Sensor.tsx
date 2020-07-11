63e0e0f8b553fe6a8af68575d209108f7afbec10
Piero Savi <piero@pierosavi.it>
2020-07-11 19:57:07 +0200

    Implement sensor drag handle

--- src/Sensor.tsx
+++ src/Sensor.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as _ from 'lodash';
 import { css, cx } from 'emotion';
-// import { stylesFactory, useTheme } from '@grafana/ui';
 import Draggable from 'react-draggable';
 import { stylesFactory } from '@grafana/ui';
 import SensorType from './Types/Sensor';
@@ -15,21 +15,42 @@ export const Sensor = (props: SensorProps) => {
   // const theme = useTheme();
   const styles = getStyles();
 
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const onMouseEnter = (event) => {
+    setIsMouseOver(true)
+  }
+
+  const onMouseLeave = (event) => {
+    setIsMouseOver(false)
+  }
+
   return (
     <>
       {props.sensor.visible && (
-        <Draggable disabled={props.draggable} bounds="#imageItBgImage">
+        <Draggable
+          disabled={props.draggable}
+          bounds="#imageItBgImage"
+          handle=".handle">
           <div
             className={cx(
-              styles.sensor,
+              styles.container,
               css`
                 color: ${props.sensor.fontColor};
                 background-color: ${props.sensor.backgroundColor};
-              `
-            )}
-          >
-            <a href={props.sensor.link}>{props.sensor.value}</a>
+              `)}
+            onMouseEnter={ onMouseEnter }
+            onMouseLeave={ onMouseLeave }>
 
+            <div className={cx(styles.content)}>
+              <a className={css`color: ${props.sensor.fontColor};`} href={props.sensor.link ? props.sensor.link : '#'}>
+                {props.sensor.value}
+              </a>
+            </div>
+
+            { isMouseOver && (
+              <div className={cx(styles.handle, "handle",)}><div className="fa fa-bars"/></div>
+            )}
           </div>
         </Draggable>
       )}
@@ -39,9 +60,16 @@ export const Sensor = (props: SensorProps) => {
 
 const getStyles = stylesFactory(() => {
   return {
-    sensor: css`
+    container: css`
       position: absolute;
       padding: 0.5em;
+    `,
+    handle: css`
+      float: right;
+      margin-left: 0.5em;
+    `,
+    content: css`
+      float: left;
     `,
   };
 });
