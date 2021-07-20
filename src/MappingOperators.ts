ef79f420aeb37ab8a7a2bfbe6df5a6987637d3eb
Piero <15440183+pierosavi@users.noreply.github.com>
2021-07-20 22:07:56 +0200

    Add not equal to mapping operator

--- src/MappingOperators.ts
+++ src/MappingOperators.ts
@@ -1,8 +1,7 @@
 import { MappingOperator } from './types/Mapping';
 
 function isEqualTo(a: number | string, b: number | string) {
-  // Maybe keep == to compare string and numbers
-  return a !== undefined && b !== undefined ? a === b : false;
+  return a !== undefined && b !== undefined ? a == b : false;
 }
 
 function isGreaterThan(a: number | string, b: number | string) {
@@ -13,12 +12,22 @@ function isSmallerThan(a: number | string, b: number | string) {
   return a !== undefined && b !== undefined ? a < b : false;
 }
 
+function isNotEqualTo(a: number | string, b: number | string) {
+  return !isEqualTo(a, b);
+}
+
 const MappingOperators: MappingOperator[] = [
   {
     id: 'equal',
     operator: '=',
     function: isEqualTo,
     description: 'Check if the two values are equal',
+  },
+  {
+    id: 'notEqual',
+    operator: '!=',
+    function: isNotEqualTo,
+    description: 'Check if the two values are not equal',
   },
   {
     id: 'greaterThan',
